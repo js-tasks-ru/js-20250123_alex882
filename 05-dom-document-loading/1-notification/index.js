2a6c3dacfc4179abc3b36259192918c396177cc1
Игнатов Алексей <a.ignatov@vr-protect.ru>
2025-02-10 16:21:32 +0300

    05-dom-document-loading

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,64 @@
 export default class NotificationMessage {
+  element;
+  static lastShownComponent;
 
+  constructor(
+    message = '',
+    {
+      duration = 0,
+      type = '',
+    } = {}
+  ) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.element = this.createElement(this.createTemplate());
+  }
+
+  show(targetElement) {
+
+    if (NotificationMessage.lastShownComponent) {
+      NotificationMessage.lastShownComponent.remove();
+    }
+
+    NotificationMessage.lastShownComponent = this;
+
+    setTimeout(() => this.remove(), this.duration);
+    if (!targetElement) {
+      document.body.append(this.element);
+    } else {
+      targetElement.append(this.element);
+    }
+  }
+
+  createElement(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+
+    return element.firstElementChild;
+  }
+
+  createTemplate() {
+    const animationDurationValue = (this.duration / 1000) + 's';
+
+    return (`
+  <div class="notification ${this.type}" style="--value:${animationDurationValue}">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+        ${this.message}
+      </div>
+    </div>
+  </div>`);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,142 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+  element;
+  subElements = {};
 
+  constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+
+    this.element = this.createElement(this.createTableTemplate());
+    this.selectSubElements();
+  }
+
+  selectSubElements() {
+    this.element.querySelectorAll('[data-element]').forEach(element => {
+      this.subElements[element.dataset.element] = element;
+    });
+  }
+
+  createElement(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+
+    return element.firstElementChild;
+  }
+
+  createTableTemplate() {
+    return (`
+  <div class="sortable-table">
+
+    <div data-element="header" class="sortable-table__header sortable-table__row">
+    ${this.createHeaderTemplate(this.headerConfig)}
+    </div>
+
+    <div data-element="body" class="sortable-table__body">
+    ${this.createBodyTemplate(this.data)}
+    </div>
+
+    <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
+
+    <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
+      <div>
+        <p>No products satisfies your filter criteria</p>
+        <button type="button" class="button-primary-outline">Reset all filters</button>
+      </div>
+    </div>
+  </div>`);
+  }
+
+  createHeaderTemplate(headerConfig) {
+    return (`
+    ${headerConfig.map(({id, title, sortable}) => this.createHeaderCellTemplate(id, title, sortable)).join(' ')}
+    `);
+  }
+
+  createHeaderCellTemplate(id, title, sortable) {
+    return (`
+       <div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}">
+        <span>${title}</span>
+        <span data-element="arrow" class="sortable-table__sort-arrow">
+            <span class="sort-arrow">
+        </span>
+      </span>
+      </div>
+    `);
+  }
+
+  createBodyTemplate(data) {
+    return (`
+    ${data.map((item) => this.createBodyRowTemplate(item)).join(' ')}
+    `);
+  }
+
+  createBodyRowTemplate(item) {
+    return (`
+    <a href="#" class="sortable-table__row">
+        ${this.createBodyCellTemplate(item)}
+      </a>
+    `);
+  }
+
+  createBodyCellTemplate(dataItem) {
+    return this.headerConfig.map(({id, template}) => {
+      if (id === 'images') {
+        return (`
+        ${template(dataItem[id])}
+`);
+      } else {
+        return (`
+      <div class="sortable-table__cell">${dataItem[id]}</div>`);
+      }
+    }).join(' ');
+  }
+
+  sort(field, direction) {
+    const newData = [...this.data];
+
+    const setDataOrder = () => {
+      const previousElements = this.element.querySelectorAll(`.sortable-table__cell[data-order][data-sortable="true"]`);
+      previousElements.forEach(item => item.dataset.order = '');
+
+      const currentElement = this.element.querySelector(`.sortable-table__cell[data-id=${field}][data-sortable="true"]`);
+      currentElement.dataset.order = direction;
+    };
+
+    if (typeof newData[0][field] === 'string') {
+
+      if (direction === 'asc') {
+        newData.sort((a, b) => a[field].localeCompare(b[field], ["ru", "eng"], { caseFirst: 'upper' }));
+        setDataOrder();
+      } else {
+        newData.sort((a, b) => b[field].localeCompare(a[field], ["ru", "eng"], { caseFirst: 'upper' }));
+        setDataOrder();
+      }
+    }
+    else if (typeof newData[0][field] === 'number') {
+
+      if (direction === 'asc') {
+        newData.sort((a, b) => a[field] - b[field]);
+        setDataOrder();
+      } else {
+        newData.sort((a, b) => b[field] - a[field]);
+        setDataOrder();
+      }
+    }
+
+    this.update(newData);
+  }
+
+  update(newData) {
+    this.data = newData;
+    this.element.querySelector('[data-element="body"]').innerHTML = this.createBodyTemplate(this.data);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
   }
 }
 
